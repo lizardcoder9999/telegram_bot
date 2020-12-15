c12513c686c61adb03aabf8d68d3c26207c6d5b9
lizardcoder9999 <lizardcoder6@gmail.com>
2020-12-15 18:46:47 -0500

    Added symbol lookup feature

--- bot.py
+++ bot.py
@@ -38,22 +38,54 @@ def welcome(message):
 
 @bot.message_handler(func = lambda msg: msg.text is not None and '!ticker=' in msg.text) 
 def get_ticker_info(message):
-    filtered_symbol = message.text.replace('!ticker=','')
-    url = f'https://api.blockchain.com/v3/exchange/tickers/{filtered_symbol}'
+    filtered_ticker = message.text.replace('!ticker=','')
+    url = f'https://api.blockchain.com/v3/exchange/tickers/{filtered_ticker}'
     res = requests.get(url)
     ticker_res = json.loads(res.text)
-    bot.reply_to(message,f"Symbol: {ticker_res['symbol']}\nPrice 24h: {ticker_res['price_24h']}\nVolume 24h: {ticker_res['volume_24h']}\nLast Trade Price: {ticker_res['last_trade_price']}")
+    try:
+        bot.reply_to(message,f"Symbol: {ticker_res['symbol']}\nPrice 24h: {ticker_res['price_24h']}\nVolume 24h: {ticker_res['volume_24h']}\nLast Trade Price: {ticker_res['last_trade_price']}")
+    except:
+        bot.reply_to(message,"Ticker not found")
+
+    
 
 
 
-#sample response
+@bot.message_handler(func = lambda msg: msg.text is not None and '!symbol=' in msg.text)
+def get_symbol_info(message):
+    filtered_symbol = message.text.replace('!symbol=','')
+    url = f"https://api.blockchain.com/v3/exchange/symbols/{filtered_symbol}"
+    res = requests.get(url)
+    symbol_res = json.loads(res.text)
+    try:
+        bot.reply_to(message,f"base currency: {symbol_res['base_currency']}\nbase currency scale: {symbol_res['base_currency_scale']}\ncounter currency: {symbol_res['counter_currency']}\ncounter currency scale: {symbol_res['counter_currency_scale']}\nmin price increment: {symbol_res['min_price_increment']}\nmin order size: {symbol_res['min_order_size']}\nmin order size scale: {symbol_res['min_order_size_scale']}\nlot size: {symbol_res['lot_size']}\nlot size scale: {symbol_res['lot_size_scale']}\nstatus: {symbol_res['status']}\nid: {symbol_res['id']}\nauction price: {symbol_res['auction_price']}\nauction size: {symbol_res['auction_size']}\nauction time: {symbol_res['auction_time']}\nimbalance: {symbol_res['imbalance']}")
+
+    except:
+        bot.reply_to(message,"Symbol not found")
+
+
+
+#example response
 # {
-#     "symbol": "BTC-USD",
-#     "price_24h": 19284.2,
-#     "volume_24h": 392.43106795,
-#     "last_trade_price": 19461.5
+#     "base_currency": "LTC",
+#     "base_currency_scale": 8,
+#     "counter_currency": "BTC",
+#     "counter_currency_scale": 8,
+#     "min_price_increment": 100,
+#     "min_price_increment_scale": 8,
+#     "min_order_size": 8800000,
+#     "min_order_size_scale": 8,
+#     "max_order_size": 0,
+#     "max_order_size_scale": 8,
+#     "lot_size": 1,
+#     "lot_size_scale": 8,
+#     "status": "close",
+#     "id": 23,
+#     "auction_price": 0.0,
+#     "auction_size": 0.0,
+#     "auction_time": "",
+#     "imbalance": 0.0
 # }
-
 
 
 bot.polling()
